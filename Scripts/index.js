fbd683668dd6d0d9ebcd91f0e213e734763e2769
JeffJacobson <jeffrey.m.jacobson@github.fake>
2013-11-04 14:35:47 -0800

    Added selection layer

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,22 +1,79 @@
 ﻿/*global require*/
 require([
-	"dojo/on",
 	"esri/map",
 	"esri/layers/GraphicsLayer",
 	"esri/tasks/GeometryService",
 	"esri/toolbars/draw",
 	"esri/tasks/QueryTask",
 	"esri/tasks/query"
-], function (on, Map, GraphicsLayer, GeometryService, Draw, QueryTask, Query) {
+], function (Map, GraphicsLayer, GeometryService, Draw, QueryTask, Query) {
 	"use strict";
-	var map, geometryService, draw, queryTask, selectedCountiesLayer;
+	var map, geometryService, draw, queryTask, serviceAreaLayer, selectionLayer;
+
+
+
+	/** 
+	 * @returns {dojo/Deferred}
+	 */
+	function queryCounties(response) {
+		var query;
+
+		query = new Query();
+		query.returnGeometry = true;
+		query.geometry = response.geometry;
+		return queryTask.execute(query);
+	}
+
+	function addSelectedCountiesToLayer(queryResponse) {
+		if (!serviceAreaLayer.graphics.length) {
+			queryResponse.features.forEach(function (feature) {
+				serviceAreaLayer.add(feature);
+			});
+		} else {
+			queryResponse.features.forEach(function (feature) {
+				selectionLayer.add(feature);
+			});
+		}
+	}
+
+	function handleError(error) {
+		console.error("error", error);
+	}
+
+	/**
+	 * @param {object} response
+	 * @param {object} response.geometry
+	 */
+	function selectCountiesOnDrawComplete(response) {
+		queryCounties(response).then(addSelectedCountiesToLayer, handleError);
+	}
+
+	function handleOnMapLoad() {
+		// Create the selected counties layer.
+		serviceAreaLayer = new GraphicsLayer({
+			id: "serviceArea",
+			className: "service-area",
+			styling: false
+		});
+
+		selectionLayer = new GraphicsLayer({
+			id: "selection",
+			className: "selection",
+			styling: false
+		});
+
+		map.addLayer(serviceAreaLayer);
+		map.addLayer(selectionLayer);
+
+		// Create the draw toolbar.
+		draw = new Draw(map);
+		draw.activate("polyline");
+		draw.on("draw-complete", selectCountiesOnDrawComplete);
+	}
 
 	geometryService = new GeometryService("http://www.wsdot.wa.gov/geosvcs/ArcGIS/rest/services/Geometry/GeometryServer");
 
 	queryTask = new QueryTask("http://www.wsdot.wa.gov/geosvcs/ArcGIS/rest/services/Shared/CountyBoundaries/MapServer/0");
-
-	
-
 
 	map = new Map("map", {
 		basemap: "gray",
@@ -25,51 +82,5 @@ require([
 		showAttribution: true
 	});
 
-	map.on("load", function () {
-
-		/** 
-		 * @returns {dojo/Deferred}
-		 */
-		function queryCounties(response) {
-			var query;
-
-			query = new Query();
-			query.returnGeometry = true;
-			query.geometry = response.geometry;
-			return queryTask.execute(query);
-		}
-
-		// Create the selected counties layer.
-		selectedCountiesLayer = new GraphicsLayer({
-			id: "selectedCounties",
-			className: "county",
-			styling: false
-		});
-
-		map.addLayer(selectedCountiesLayer);
-
-		function addSelectedCountiesToLayer(queryResponse) {
-			queryResponse.features.forEach(function (feature) {
-				selectedCountiesLayer.add(feature);
-			});
-		}
-
-		function handleError(error) {
-			console.error("error", error);
-		}
-
-		/**
-		 * @param {object} response
-		 * @param {object} response.geometry
-		 */
-		function selectCountiesOnDrawComplete(response) {
-			queryCounties(response).then(addSelectedCountiesToLayer, handleError);
-		}
-
-		// Create the draw toolbar.
-		draw = new Draw(map);
-		draw.activate("polyline");
-		on.once(draw, "draw-complete", selectCountiesOnDrawComplete);
-
-	});
+	map.on("load", handleOnMapLoad);
 });
