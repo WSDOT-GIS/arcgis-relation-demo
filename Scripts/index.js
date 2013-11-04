8dbdd0a80f6219ad3aad3f42f71ce75b3a45b68f
JeffJacobson <jeffrey.m.jacobson@github.fake>
2013-11-04 11:19:04 -0800

    Added files to repository.

---
+++ Scripts/index.js
@@ -0,0 +1,24 @@
+﻿/*global require*/
+require([
+	"esri/map",
+	"esri/tasks/GeometryService",
+	"esri/toolbars/draw"
+], function (Map, GeometryService, Draw) {
+	"use strict";
+	var map, geometryService, draw;
+
+	geometryService = new GeometryService("http://www.wsdot.wa.gov/geosvcs/ArcGIS/rest/services/Geometry/GeometryServer");
+
+
+	map = new Map("map", {
+		basemap: "gray",
+		center: [-120.80566406246835, 47.41322033015946],
+		zoom: 7,
+		showAttribution: true
+	});
+
+	map.on("load", function () {
+		// Create the draw toolbar.
+		draw = new Draw(map);
+	});
+});
